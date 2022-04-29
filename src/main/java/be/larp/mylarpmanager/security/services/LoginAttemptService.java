ee9f00ff78f118cb70e6757b6c36b4f507c758a0
Tom Magis <tommagisit@gmail.com>
2022-04-29 11:18:14 +0200

    Adds prevention for bruteforce

---
+++ src/main/java/be/larp/mylarpmanager/security/services/LoginAttemptService.java
@@ -0,0 +1,60 @@
+package be.larp.mylarpmanager.security.services;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class LoginAttemptService {
+
+    Logger logger = LoggerFactory.getLogger(LoginAttemptService.class);
+
+    @Value("${warden.app.tentatives.maxTentatives:10}")
+    private int maxTentatives;
+    private final LoadingCache<String, Integer> attemptsCache;
+
+    @Autowired
+    public LoginAttemptService(@Value("${warden.app.tentatives.gracePeriodInMinutes:60}") int gracePeriodInMinutes) {
+        super();
+        logger.info("Creating");
+        attemptsCache = CacheBuilder.newBuilder().expireAfterWrite(gracePeriodInMinutes, TimeUnit.MINUTES).build(new CacheLoader<>() {
+            @Override
+            public Integer load(String key) {
+                return 0;
+            }
+        });
+    }
+
+    public void loginSucceeded(String key) {
+        logger.trace("Login succeeded for " + key);
+        attemptsCache.invalidate(key);
+    }
+
+    public void loginFailed(String key) {
+        logger.trace("Login failed for " + key);
+        int attempts;
+        try {
+            attempts = attemptsCache.get(key);
+        } catch (ExecutionException e) {
+            attempts = 0;
+        }
+        attempts++;
+        attemptsCache.put(key, attempts);
+    }
+
+    public boolean isBlocked(String key) {
+        try {
+            return attemptsCache.get(key) >= maxTentatives;
+        } catch (ExecutionException e) {
+            return false;
+        }
+    }
+}
--- src/main/java/be/larp/mylarpmanager/security/services/UserDetailsServiceImpl.java
+++ src/main/java/be/larp/mylarpmanager/security/services/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package be.larp.mylarpmanager.security.services;
 
+import be.larp.mylarpmanager.controllers.Controller;
 import be.larp.mylarpmanager.models.User;
 import be.larp.mylarpmanager.repositories.UserRepository;
 import org.slf4j.Logger;
@@ -10,17 +11,32 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    Logger logger = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    private LoginAttemptService loginAttemptService;
+
+    @Autowired
+    private HttpServletRequest request;
 
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        String ip = getClientIP();
+        if (loginAttemptService.isBlocked(ip)) {
+            logger.info("Blocked request for IP " + ip);
+            throw new RuntimeException("blocked");
+        }else{
+            logger.trace("Non blocked request for IP " + ip);
+        }
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User with username " + username + " not found."));
     }
@@ -31,8 +47,16 @@ public class UserDetailsServiceImpl implements UserDetailsService {
                 .orElseThrow(() -> new UsernameNotFoundException("User with uuid " + uuid + " not found."));
     }
 
-    public String getCurrentToken(String userUuid) throws UsernameNotFoundException{
+    public String getCurrentToken(String userUuid) throws UsernameNotFoundException {
         return userRepository.findByUuid(userUuid)
                 .orElseThrow(() -> new UsernameNotFoundException("User with uuid " + userUuid + " not found.")).getCurrentToken();
+    }
+
+    private String getClientIP() {
+        String xfHeader = request.getHeader("X-Forwarded-For");
+        if (xfHeader == null) {
+            return request.getRemoteAddr();
+        }
+        return xfHeader.split(",")[0];
     }
 }
